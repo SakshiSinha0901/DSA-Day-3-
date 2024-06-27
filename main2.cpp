5cda73b4350a5517fc74a961a4d16e9273447f91
Sakshi Sinha <128817418+SakshiSinha0901@users.noreply.github.com>
2024-06-27 22:12:31 +0530

    Update main2.cpp

--- main2.cpp
+++ main2.cpp
@@ -76,4 +76,73 @@ int main(){
         cin >>arr[i];
     }
     cout<< "Maximum number is:" << findMax(arr,size)<<endl;
-}        
+} 
+
+// Counting how many zero and ones are present.
+// #include <iostream>
+// #include <limits.h>
+// using namespace std;
+
+// void printzeroAndones(int arr[],int n){
+//     int zeroCount = 0;
+//     int onesCount = 0;
+//     // traverse in whole array
+//     for(int i; i<n ;i++){
+//         int currElement = arr[i];
+//         if (currElement == 0){
+//             zeroCount++ ;
+//         }
+//         if (currElement ==1){
+//             onesCount++ ;
+//         }
+//     }
+//     cout<<"Total Zeroes:" <<zeroCount<<endl;
+//     cout<<"Total Ones:" <<onesCount<<endl;
+// }
+// int main(){
+//     int arr[100];
+
+//     int size;
+//     cout<<"Enter the number of elements:"<<endl;
+//     cin>>size;
+
+//     for(int i = 0; i<size; i++ ){
+//         cout<<"Enter the input value for index : "<<i<<endl;
+//         cin >>arr[i];
+//     }
+
+//     printzeroAndones(arr,size);
+// }
+
+
+// Doing extreme count using two pointer for both odd and even 
+#include <iostream>
+using namespace std;
+void extremeprint(int arr[],int n){
+    int i = 0; // left side
+    int j = n-1; // right side
+    while( i<=j){
+        // if(i==j){
+        //     cout<<arr[i]<<" ";
+        //     i++;
+        // }
+        // else{
+        //     cout<<arr[i]<<" ";
+        //     i++ ;
+        //     cout<<arr[j]<<" ";
+        //     j-- ;
+        // }
+        //OR
+        cout<<arr[i]<<" ";
+        i++;
+        if(j>i){
+            cout<<arr[j]<<" ";
+            j-- ;
+        }
+    }
+}
+int main(){
+    int arr[] = {10,20,30,40,50,60,70};
+    int size = 7;
+    extremeprint(arr,size) ;
+}
